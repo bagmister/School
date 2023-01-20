de89c57a11b3f0aad30592240f0acab4c576917a
kevin bagley <kevin.bagley@progleasing.com>
2023-01-19 19:57:22 -0700

    week 3 work for team

--- CES111/Week3/fitness.py
+++ CES111/Week3/fitness.py
@@ -1,10 +1,4 @@
-
-
-
-# Import datetime so that it can be
-# used to compute a person's age.
 from datetime import datetime
-
 
 def main():
     # Get the user's gender, birthdate, height, and weight.
@@ -16,25 +10,13 @@ def main():
     killograms_weight = kg_from_lb(weight)
     height_in_centimeters = cm_from_in(height)
     bmr = basal_metabolic_rate(age=age,gender=gender, weight=killograms_weight, height=height_in_centimeters) # must calcluate different for M and F
-    bmi = body_mass_index(weight=killograms_weight, height=height_in_centimeters, age=age, gender=gender)
-
-    # Print the results for the user to see.
+    bmi = body_mass_index(weight=killograms_weight, height=height_in_centimeters)
     pass
 
-
 def compute_age(birth_str):
-    """Compute and return a person's age in years.
-    Parameter birth_str: a person's birthdate stored
-        as a string in this format: YYYY-MM-DD
-    Return: a person's age in years.
-    """
-    # Convert a person's birthdate from a string
-    # to a date object.
     birthdate = datetime.strptime(birth_str, "%Y-%m-%d")
     today = datetime.now()
 
-    # Compute the difference between today and the
-    # person's birthdate in years.
     years = today.year - birthdate.year
 
     # If necessary, subtract one from the difference.
@@ -42,43 +24,33 @@ def compute_age(birth_str):
         (birthdate.month == today.month and \
             birthdate.day > today.day):
         years -= 1
-
+    print(f"Based on what you entered your age is {years}.")
     return years
-
 
 def kg_from_lb(pounds):
     lb_in_kg= pounds * .45359237
+    
+    print(f"Your weight in Killograms is: {lb_in_kg}")
     return lb_in_kg
-
 
 def cm_from_in(inches):
     converted_inch_to_cent = inches * 2.54 
+    
+    print(f"Your hight in Centemeters is {converted_inch_to_cent}. Based on the number in inches you provided.")
     return converted_inch_to_cent
 
-
-def body_mass_index(weight, height, gender, age):
-
-    if gender == 'f':
-        bmr = 447.593 + 9.247 weight + 3.098 height - 4.330 age
-    else:
-        bmr = 88.362 + 13.397 weight + 4.799 height - 5.677 age
-
-    return bmr
-
+def body_mass_index(weight, height):
+    bmi = 10000 * weight / height **2
+    print(f"Your Body Mass Index is {bmi:.2f} based on your height and weight entered.")
+    return bmi
 
 def basal_metabolic_rate(gender, weight, height, age):
-    """Compute and return a person's basal metabolic rate.
-    Parameters
-        weight: a person's weight in kilograms.
-        height: a person's height in centimeters.
-        age: a person's age in years.
-    Return: a person's basal metabolic rate in kcals per day.
-    """
+    if gender == 'f':
+        bmr = 447.593 + (9.247 *weight) + (3.098 * height) - (4.330 * age)
+    else:
+        bmr = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age)
+    print(f"Your Basal Metabolic Rate based on you being a {gender} (f for female and m for male) is {bmr:.2f}.")
+
     return
 
-
-
-
-# Call the main function so that
-# this program will start executing.
-
+main()
