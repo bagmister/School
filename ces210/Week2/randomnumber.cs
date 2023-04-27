394d97f69179b7c033e478d72923228ff55f3041
kevin bagley <kevin.bagley@progleasing.com>
2023-04-26 21:56:57 -0600

    update

---
+++ ces210/Week2/randomnumber.cs
@@ -0,0 +1,15 @@
+using System;
+
+
+class MagicNumber
+{
+    static void Main(string[] args)
+    {
+        var magicInt = new Random();
+        var number = magicInt.Next(1,11);
+        Console.WriteLine("Hello Prep1 World!");
+        //Console.ReadLine();
+
+
+    }
+}
