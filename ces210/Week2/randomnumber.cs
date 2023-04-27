02a03a8cce9661d394fff8f196127231059f2102
kevin bagley <kevin.bagley@progleasing.com>
2023-04-26 22:52:57 -0600

    update

--- ces210/Week2/randomnumber.cs
+++ ces210/Week2/randomnumber.cs
@@ -5,11 +5,26 @@ class MagicNumber
 {
     static void Main(string[] args)
     {
-        var magicInt = new Random();
-        var number = magicInt.Next(1,11);
-        Console.WriteLine("Hello Prep1 World!");
-        //Console.ReadLine();
+        var magicRandmom = new Random();
+        var Magicnumber = magicRandmom.Next(1,101);
+        var UserGuess = -1;
+        while (UserGuess != Magicnumber)
+        {
+            Console.Write("What is your guess?");
+            UserGuess = int.Parse(Console.ReadLine());
+            if (UserGuess > Magicnumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (UserGuess < Magicnumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You Guessed it!");
+            }
 
-
+        }
     }
 }
